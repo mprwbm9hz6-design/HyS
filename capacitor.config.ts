e322fa6a72e04d9436a5f37db32da8cea9cfdf73
mprwbm9hz6-design <mprwbm9hz6@privaterelay.appleid.com>
2026-09-09 19:17:07 -0300

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'ar.com.informehys.app',
-  appName: 'Informe HyS',
-  webDir: 'www'
-};
-
-export default config;
